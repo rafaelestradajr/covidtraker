da31603337ade5882abf711e6faf0a5958d7c447
Rafael Estrada <rafaelestrada@comcast.net>
2021-04-25 22:50:13 -0700

    added button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <main v-if="!loading">
-Show Data
+<DataTitle :text='title' :dataDate='dataDate'/>
+<DataBoxes :stats='stats/>
+<CountrySelect @get-country='getCountryData' :countries='countries'/>
+<button 
+@click='clearCountryData'
+v-if'stats.Country'
+class=bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600'
+>
+Clear Country 
+</button>
 </main>
 
 <main class ="flex flex-col align-center justify-center text-center" v-else>
@@ -13,17 +22,24 @@ Fetching Data
 </template>
 
 <script>
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+  DataTitle,
+  DataBoxes,
+  CountrySelect,
+  },
   data(){
   return {
   loading: true,
   title: 'Global',
   dataDate:'',
   stats:{},
-  countries: {},
+  countries: [],
   loadingImage: require('../assets/hourglass.gif')
   }
   },
@@ -32,12 +48,24 @@ export default {
   const res = await fetch("https://api.covid19api.com/summary")
   const data = await res.json()
   return data
-  }
+  
+  },
+  getCountryData(country) {
+  this.stats=country
+  this.title=country.Country
+  },
+  async clearCountryData(){
+  this.loading=true
+  const data = await this.fetchCovidData()
+  this.title='Global'
+  this.stats=data.Global
+  this.loading=false
+  },
   },
 
   async created() {
 const data = await this.fetchCovidData()
-this.dataDate =date.Date
+this.dataDate =data.Date
 this.stats = data.Global
 this.countries= data.Countries
 this.loading = false
